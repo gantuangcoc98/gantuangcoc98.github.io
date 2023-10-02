a67c68fa7fce47f8c9d8f15501bd9483a3a506f2
gantuangcoc98 <gantuangco98@yahoo.com.ph>
2023-10-02 14:43:52 +0800

    updated changes

--- eCommerceProject/app.js
+++ eCommerceProject/app.js
@@ -4,9 +4,15 @@ company_title.addEventListener('click', () => {
     window.location.href = "index.html";
 });
 
-const search_symbol = document.getElementById('search_symbol');
 const search_input = document.getElementById('search_input');
-
+const search_symbol = document.getElementById('search_symbol');
 search_symbol.addEventListener('click', () => {
     search_input.focus();
+});
+
+
+const browse_search_input = document.getElementById('browse_search_input');
+const browse_search_symbol = document.getElementById('browse_search_symbol');
+browse_search_symbol.addEventListener('click', () => {
+    browse_search_input.focus();
 });
